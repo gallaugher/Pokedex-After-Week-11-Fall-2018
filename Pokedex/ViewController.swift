5619e8818948538a83f911d1c5e5c662b13d2a2f
John Gallaugher <john.gallaugher@bc.edu>
2018-11-06 19:07:18 -0500

    Code at the end of class Week 11 - Nov. 6 2018

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var pokemon = Pokemon()
+    var pokeArray = ["Bulbasaur",
+                     "Pikachu",
+                     "Snorlax",
+                     "Wigglytuff",
+                     "Charmander"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        pokemon.getPokemon() {
+            self.tableView.reloadData()
+        }
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemon.pokeArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = pokemon.pokeArray[indexPath.row].name
+        return cell
+    }
+}
